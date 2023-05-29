7f71f78d955e0d6c9f86812e7f7589f609940512
LisaBagszis <lisa.bagszis@gmail.com>
2023-05-29 23:24:02 +0200

    bl

--- src/main/java/org/example/PasswordValidationFinal.java
+++ src/main/java/org/example/PasswordValidationFinal.java
@@ -4,7 +4,7 @@ public class PasswordValidationFinal {
     public static void main(String[] args) {
 
 
-        System.out.println(checkTheLengthAndTheContent("Passwort12"));
+        System.out.println(checkTheLengthAndTheContent("abc"));
 
     }
 
